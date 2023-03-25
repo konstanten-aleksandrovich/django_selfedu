da8bf34fb95e65da3fbb06304d73e4733d9fd726
Konstantin <ckammmmm@gmail.com>
2023-03-25 13:08:00 +0500

    4 урок

--- coolsite/coolsite/urls.py
+++ coolsite/coolsite/urls.py
@@ -15,11 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+
+from coolsite import settings
 from women.views import index,categories,pageNotFound,ServerError
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('women.urls')),
 ]
+
+
+
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 handler404 = pageNotFound
 handler500=ServerError
