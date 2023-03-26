17eaed433b764a0369c638a80efda36501a16099
Konstantin <ckammmmm@gmail.com>
2023-03-26 13:02:30 +0500

    9 занятие

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -11,10 +11,13 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
 
 def index(request):
     posts=Women.objects.all()
+    cats=Category.objects.all()
     context = {
         'posts': posts,
+        'cats':cats,
         'menu': menu,
-        'title': 'Главная страница'
+        'title': 'Главная страница',
+        'cat_selected': 0,
     }
     return render(request,'women/index.html',context=context)
 def about(request):
@@ -27,6 +30,19 @@ def login(request):
     return HttpResponse('Авторизация')
 def show_post(request,post_id):
     return HttpResponse(f'отоброжение статьи  id={post_id}')
+def show_category(request,cat_id):
+    posts = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+    if len(posts) == 0:
+        raise Http404()
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Отоброжение по рубрикам',
+        'cat_selected': cat_id,
+    }
+    return render(request, 'women/index.html', context=context)
 
 
 
